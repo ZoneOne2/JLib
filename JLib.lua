3d1f3c0d8f0f803dd119a4c7df7dc4c0ce500cee
ZoneOne2 <ZoneOne2Design@gmail.com>
2014-07-24 18:35:04 -0400

    JLib
    Updated play.bat and conf.lua for latest version of Love (0.9.1)

---
+++ JLib.lua
@@ -0,0 +1,207 @@
+
+function init()
+	lg = love.graphics
+	
+	window = {}
+	window.width = lg.getWidth()
+	window.height = lg.getHeight()
+end
+
+function findIntersect(L1,L2)
+
+--FIX THIS.
+	L1.x1 = L1[1][1]
+	L1.y1 = L1[1][2]
+	L1.x2 = L1[2][1]
+	L1.y2 = L1[2][2]
+	
+	L2.x1 = L2[1][1]
+	L2.y1 = L2[1][2]
+	L2.x2 = L2[2][1]
+	L2.y2 = L2[2][2]
+	--check for horizontal/vertical lines
+
+	L1.slope = findSlope({L1.x1,L1.y1},{L1.x2,L1.y2})
+	L2.slope = findSlope({L2.x1,L2.y1},{L2.x2,L2.y2})
+
+	return L2.slope
+
+end
+
+function findSlope(p1,p2)
+
+--FIX THIS.
+	p1.x=p1[1]
+	p1.y=p1[2]
+	p2.x=p2[1]
+	p2.y=p2[2]
+	
+	--check if points are the same
+	if (p1.x==p2.x and p1.y==p2.y) then
+	
+		return 0
+	
+	--check if line is vertical
+	elseif (p1.x == p2.x) then
+	
+		return "inf"
+	
+	else
+	
+		return (p2.y-p1.y)/(p2.x-p1.x)
+	
+	end
+	
+
+end
+
+--p2 is apex of angle
+--p1=alpha/a, p2=gamma/c, p3=beta/b
+function findAngle(p1,p2,p3)
+
+--FIX THIS.
+	p1.x=p1[1]
+	p1.y=p1[2]
+	p2.x=p2[1]
+	p2.y=p2[2]
+	p3.x=p3[1]
+	p3.y=p3[2]
+	
+	a = dist(p2,p3)
+	b = dist(p1,p2)
+	c = dist(p1,p3)
+	
+	--check for case where p1 is the same as p2
+	if (p1.x==p2.x and p1.y==p2.y) then
+	
+		gamma = 0
+		
+	else
+	
+		gamma = math.acos( ((a^2)+(b^2)-(c^2)) / (2*a*b) )
+		
+	end
+		
+	return gamma
+
+end
+
+function dist(p1,p2)
+	
+	return math.sqrt( ((p2[1]-p1[1])^2) + ((p2[2]-p1[2])^2) )
+
+end
+
+function mergeTables(...)
+	merged = {}
+	
+	for i=1, #arg do
+		for j, item in pairs(arg[i]) do
+			table.insert(merged,item)
+		end
+	end
+	
+	return merged
+
+end
+
+function printTable(t)
+	
+	text = ""
+	
+	for i, item in pairs(t) do
+			text = text..item.."\t"
+	end
+	
+	print(text)
+	
+end
+
+function pointLine(...)
+
+	lineTable = {}
+	
+	for i=1, #arg do
+		lineTable = mergeTables(lineTable,arg[i])
+	end
+	
+	lg.line(lineTable)
+
+end
+
+--arg[1] is type, rest are vertices
+function pointPolygon(...)
+
+	polyTable = {}
+	
+	for i=2, #arg do
+		polyTable = mergeTables(polyTable,arg[i])
+	end
+	
+	lg.polygon(arg[1],polyTable)
+
+end
+
+function adv(var, dist)
+
+	return var + dist
+
+end
+
+function advCirc(var, dist, limit)
+	
+	retVal = (var+dist)%limit
+	
+	if retVal == 0 then
+	
+		return limit
+	
+	else
+	
+		return retVal
+		
+	end
+
+end
+
+--takes tables of points
+--arg[1] is point in question, rest are possible points
+function findClosestPoint(...)
+	
+	p = {}
+	p[1] = arg[1][1]
+	p[2] = arg[1][2]
+
+	--start by assuming first in points list (arg[2]) is closest
+	closest = {arg[2][1], arg[2][2]}
+	
+	
+	closestDist = dist(p,arg[2])
+
+
+	
+	--next check 3 through...
+	for i=3, #arg do
+	
+		if (dist(p,arg[i]) < closestDist) then
+			closest = {arg[i][1], arg[i][2]}
+			closestDist = dist(p,arg[i])
+		end
+	
+	end
+	
+	return closest
+
+end
+
+
+
+
+function pause()
+
+	print("Paused.")
+	
+	
+	while(true)do end
+	
+end
---
+++ main.lua
@@ -0,0 +1,88 @@
+function love.load()
+	require "JLib"
+	init()
+	
+	pointTypes = {"polygon", "angle", "center"}
+	activePointType = 1
+	
+	t = {}
+	t.polygon = {}
+	t.angle = {}
+	
+	
+	
+end
+
+function love.draw()
+
+	lg.push()
+	
+	
+	
+		lg.scale(1,-1)
+		lg.translate(window.width/2,-window.height/2)
+
+		lg.setColor(255,0,0)
+		lg.circle("fill",0,0,10)
+		
+		lg.setColor(0,255,0)
+		lg.circle("fill",100,0,10)
+		
+		lg.setColor(0,0,255)
+		lg.circle("fill",100,100,10)
+
+		lg.setColor(255,0,255)
+		lg.circle("fill",0,100,10)
+		
+	
+		lg.setColor(0,255,0)
+		pointLine(t1,t2)
+		pointLine(t2,t3)
+		
+		lg.setColor(255,0,0)
+		--pointPolygon("line",t1,t2,t3)
+	
+	
+	lg.pop()
+
+end
+
+function love.update(dt)
+	
+	mx = love.mouse.getX()-(window.width/2)
+	my = -love.mouse.getY()+(window.height/2)
+	
+	
+	t1 = {mx,my}
+	t2 = {97,5}
+	t3 = {26,31}
+	
+
+	test = findClosestPoint(t1,t2,t3)
+
+	printTable(test)
+	
+	--pause()
+	
+end
+
+function love.focus(bool)
+end
+
+function love.keypressed( key, unicode )
+	if key == "tab" then
+		activePointType = advCirc(activePointType,1,#pointTypes)
+	end
+end
+
+function love.keyreleased( key, unicode )
+end
+
+function love.mousepressed( x, y, button )
+end
+
+function love.mousereleased( x, y, button )
+end
+
+function love.quit()
+end
